4400e18e184160e273f8e239a163793f1a914b96
Frants <118231497+franik7@users.noreply.github.com>
2023-02-26 17:22:57 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html lang="en">
+<!-- Head -->
 
 <head>
 	<meta charset="utf-8">
@@ -11,18 +12,22 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300&display=swap" rel="stylesheet">
-
 </head>
 
+<!-- End of Head -->
+
+<!-- Nav Section Partial -->
 <?php require_once('assets/partials/nav.php');?>
-<hr class = "midsectionhr">
+<hr class="midsectionhr">
+
+<!-- Nav Section Partial End -->
+
+
+<!-- Start of Top Section /Image + Form/  -->
 
 <body>
 	<div class="container-fluid ">
-
 		<div class="row info order">
-
-
 			<br>
 			<div class="col-lg-6 half-left">
 				<form action="assets/process.php" method="post" enctype="multipart/form-data" class="needs-validation" novalidate>
@@ -30,7 +35,6 @@
 						<div class="col mb-1 ">
 							<h2><b>What We Do</b></h2>
 							<p>Life is not always easy. If you need a human touch, let us know. Fill out the form below and we will mail you a letter or a postcard at no cost. Your nickname, country, and picture (if you choose to upload one) will be displayed on our History page. This is it.</p>
-
 							<h2><b>Enter Your Info</b></h2>
 							<label for="validationCustom01">Name</label>
 							<input type="text" name="name" class="form-control" id="validationCustom01" placeholder="First and last name" value="">
@@ -48,10 +52,8 @@
 								Please provide your nickname
 							</div>
 						</div>
-
 					</div>
 					<div class="form-row">
-
 						<div class="col mb-1">
 							<label for="validationCustom03">Address</label><span style="color: red !important; display: inline; float: none;">*</span>
 							<input type="text" name="address" class="form-control" id="validationCustom03" placeholder="Address" required>
@@ -59,9 +61,16 @@
 								Please provide your address
 							</div>
 						</div>
+						<div class="col mb-1">
+							<label for="validationCustom03">What is bothering you?</label><span style="color: red !important; display: inline; float: none;">*</span>
+							<input type="text" name="question" maxlength="200" class="form-control" id="validationCustom03" placeholder="Please write what bothers you" required>
+							<div class="invalid-feedback">
+								Please write what bothers you
+							</div>
+						</div>
 
-<?php require_once('assets/partials/country.php');?>
 
+						<?php require_once('assets/partials/country.php');?>
 
 
 					</div>
@@ -83,91 +92,57 @@
 					<br>
 					<button class="btn btn-primary" type="submit">Submit form</button>
 				</form>
-
-
-
-				<!--
-
-				Your name:
-				<input type="text" name="name" placeholder="John Smith">
-				<br><br>
-				Your address:
-				<input type="text" name="address" placeholder="John Smith">
-				<br><br>
-				What would you like to recieve?
-				<br>
-				<label>
-					<input type="radio" name="letterorcard" value="Letter"> Letter
-				</label>
-				<br>
-
-				<label>
-					<input type="radio" name="letterorcard" value="Postcard"> Postcard
-				</label>
-				<br>
-
-				<br>
-				<label>
-					<input type="file" name="pic">
-				</label>
-				<br><br>
-				<input type="submit" value="Submit">
--->
-
-
-
 			</div>
 			<div class="col-lg-6 g-0 half-right">
 				<img src=assets/img/banner.jpg>
 			</div>
-
 		</div>
-
 	</div>
-<hr class = "midsectionhr">
-<!-- start of middle section -->
+	<hr class="midsectionhr">
+	<!-- End of Top Section /Image + Form/  -->
+
+
+	<!-- Start of Middle Section / Image + Text -->
 	<div class="container-fluid">
-		 <div class="row info">
-	<div class="col-lg-5 g-0 half-left-middle">
-	  <img src = "assets/img/image1.jpg">
-	</div>
-	<div class="col-lg-1">
-
-	</div>
-
-	<div class="col-lg-6 half-right-middle">
-		<p>“The human touch is that little snippet of physical affection that brings a bit of comfort, support, and kindness. It doesn’t take much from the one who gives it, but can make a huge difference in the one who receives it.” </p>
-	</div>
+		<div class="row info">
+			<div class="col-lg-5 g-0 half-left-middle">
+				<img src="assets/img/image1.jpg">
+			</div>
+			<div class="col-lg-1">
+			</div>
+			<div class="col-lg-6 half-right-middle">
+				<p>“The human touch is that little snippet of physical affection that brings a bit of comfort, support, and kindness. It doesn’t take much from the one who gives it, but can make a huge difference in the one who receives it.” </p>
+			</div>
 		</div>
-
 	</div>
-<!-- end of middle section -->
-<hr class = "midsectionhr">
-<!-- start of bottom section -->
+	<hr class="midsectionhr">
+	<!-- End of Middle Section / Image + Text -->
+
+
+	<!-- Start of Bottom Section / Image + Text -->
 	<div class="container-fluid">
-		 <div class="row info order">
-
-
-				 <div class="col-lg-5 half-right-middle">
-		<p>“During bad circumstances, which is the human inheritance, you must decide not to be reduced. You have your humanity, and you must not allow anything to reduce that. We are obliged to know we are global citizens. Disasters remind us we are world citizens, whether we like it or not.” </p>
-	</div>
-	<div class="col-lg-1">
-
-	</div>
-	<div class="col-lg-6 g-0 half-left-middle">
-	  <img src = "assets/img/image2.jpg">
-	</div>
-
+		<div class="row info order">
+			<div class="col-lg-5 half-right-middle">
+				<p>“During bad circumstances, which is the human inheritance, you must decide not to be reduced. You have your humanity, and you must not allow anything to reduce that. We are obliged to know we are global citizens. Disasters remind us we are world citizens, whether we like it or not.” </p>
+			</div>
+			<div class="col-lg-1">
+			</div>
+			<div class="col-lg-6 g-0 half-left-middle">
+				<img src="assets/img/image2.jpg">
+			</div>
 		</div>
-
 	</div>
-<!-- end of bottom section -->
 
+	<!-- End of Bottom Section / Image + Text -->
 
-
+	<!-- Footer -->
 
 	<?php require_once('assets/partials/footer.php');?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+	<!-- End of Footer -->
+
+	<!-- JS -->
 
 	<script>
 		// Example starter JavaScript for disabling form submissions if there are invalid fields
@@ -188,11 +163,9 @@
 				});
 			}, false);
 		})();
-
 	</script>
 
+	<!-- JS End -->
+
 </body>
-
-
-
 </html>
