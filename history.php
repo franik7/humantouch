7676dd3f483e45f7be12b2cdddc6901174d471fb
Frants <118231497+franik7@users.noreply.github.com>
2023-05-10 21:52:50 -0400

    Add files via upload

--- history.php
+++ history.php
@@ -35,39 +35,41 @@ how to implement default image if no image is selected
 				<th>Country</th>
 				<th>Sent</th>
 			</tr>
+
+
 			<?php
-		$d = file_get_contents("assets/data.json");
-		$d = json_decode($d,true);
+$d = file_get_contents("assets/data.json");
+$d = json_decode($d, true);
 
-		foreach($d as $i => $v){
-		echo'
-		<tr>
-			<td>'.$i.'</td>';
+foreach ($d as $i => $v) {
+	echo '<tr>';
+	echo '<td>' . htmlspecialchars($i) . '</td>';
 
-			if( substr($v['img'], -3, 3) == 'jpg' || substr($v['img'], -3, 3) == 'png' || substr($v['img'], -3, 3) == 'gif' || substr($v['img'], -4, 4) == 'jpeg' || substr($v['img'], -4, 4) == 'webp') {
-				echo '<td><img src="assets/'.$v['img'].'"></td>';
-			}
-			else {
-				echo '<td><img src="assets/img/userimgs/defaultavatar.png"></td>';
-			}
-			;
-			echo '
-			<td>'.$v["nickname"].'</td>
-			<td>'.$v["country"].'</td>
+	if (
+		substr($v['img'], -3, 3) == 'jpg' ||
+		substr($v['img'], -3, 3) == 'png' ||
+		substr($v['img'], -3, 3) == 'gif' ||
+		substr($v['img'], -4, 4) == 'jpeg' ||
+		substr($v['img'], -4, 4) == 'webp'
+	) {
+		echo '<td><img src="assets/' . htmlspecialchars($v['img']) . '"></td>';
+	} else {
+		echo '<td><img src="assets/img/userimgs/defaultavatar.png"></td>';
+	}
 
-		';
-			if($v["sent"]){
-				echo '<td><i class="fa-regular fa-square-check fa-3x"></i>
-		 </td>';
-			}else{
-			 echo '<td><i class="fa-regular fa-square fa-3x"></i>
-		 </td>';
+	echo '<td>' . htmlspecialchars($v["nickname"]) . '</td>';
+	echo '<td>' . htmlspecialchars($v["country"]) . '</td>';
 
-			};
-			echo '</tr>';
+	if ($v["sent"]) {
+		echo '<td><i class="fa-regular fa-square-check fa-3x"></i></td>';
+	} else {
+		echo '<td><i class="fa-regular fa-square fa-3x"></i></td>';
+	}
 
-		};
-		?>
+	echo '</tr>';
+}
+?>
+
 		</table>
 	</div>
 	<br>
@@ -78,6 +80,7 @@ how to implement default image if no image is selected
 	<?php require_once('assets/partials/footer.php');?>
 
 		<div class = "mid">Website by <a class = "websitelink" href='http://frants.netlify.app' target="blank">Frants</a>
+		</div>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
